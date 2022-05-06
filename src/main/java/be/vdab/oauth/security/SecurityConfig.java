9676d3627cdfee4f6250c523588d872bb58f9798
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-06 08:07:30 +0000

    v1

--- src/main/java/be/vdab/oauth/security/SecurityConfig.java
+++ src/main/java/be/vdab/oauth/security/SecurityConfig.java
@@ -1,2 +1,17 @@
-package be.vdab.oauth.security;public class SecurityConfig {
+package be.vdab.oauth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    public SecurityFilterChain geefRechten(HttpSecurity http) throws Exception {
+        http.oauth2Login();
+        http.authorizeRequests(requests -> requests.mvcMatchers("/").permitAll()
+                .mvcMatchers("/beveiligd").authenticated());
+        return http.build();
+    }
 }
